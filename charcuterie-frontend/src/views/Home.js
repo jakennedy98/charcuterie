b8e76b6d339dffbc0c37ae6d4ebabc28e204a795
Jacob Kennedy <jakennedy98@gmail.com>
2022-01-06 00:52:40 +0100

    created home page

---
+++ charcuterie-frontend/src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+        <div>
+            <img src="/images/charcuterie.jpg" alt="image" width = "100%" opacity = "0.1" />
+            
+        </div>
+    )
+}
+
+export default Home
